d2b6b158a6a768a85e65c91122cdc3650d0a7125
jimendoza <90921656+jimendoza@users.noreply.github.com>
2024-07-24 13:01:48 +0000

    Committing staged and intermediate models

---
+++ bq_dbt/models/int_ballot_returns_with_voterfile.sql
@@ -0,0 +1,18 @@
+with voterfile as (
+    select * from {{ ref('stg_ga_targetsmart_voter_base') }}
+)
+
+, ballot_returns as (
+    select * from {{ ref('stg_ga_distances_ga_early_ballot_returns') }}
+)
+
+, joined as (
+    select
+    ballot_returns.voterbase_id
+    , ballot_returns.aug_primary_voted_date
+    , voterfile.vb_vf_cd
+    from ballot_returns
+    left join voterfile using(voterbase_id)
+)
+
+select * from joined 
