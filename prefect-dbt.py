9be6a1f139cd85306b4389ac4a2e6a89aa750da6
d-reyes10 <diego@standupamerica.com>
2024-08-09 00:01:07 +0000

    update

--- prefect-dbt.py
+++ prefect-dbt.py
@@ -12,14 +12,6 @@ def dbt_flow():
 
 
 if __name__ == "__main__":
-    flow.from_source(
-        source="https://github.com/Generation-Data/dbt-tutorials.git",
-        entrypoint="prefect-dbt.py:dbt_flow"
-    ).deploy(
-        name="measurestudio-syncs",
-        work_pool_name="bigquery-load",
+    dbt_flow.serve(
         cron="0 08 * * 1" ## 8AM every Monday
 )
-    # run_dbt_job()
-    # dbt_flow()
-    # dbt_flow()
